7767b69744a1a94824cf9b5c7d7935c42c62aeb1
Pauline <pauline.auda@free.fr>
2023-10-29 11:34:48 +0100

    interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,8 +7,8 @@ import { FooterComponent } from './footer/footer.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { MenuComponent } from './menu/menu.component';
 import { ReserverTableComponent } from './reserver-table/reserver-table.component';
-import { HttpClient, HttpClientModule } from "@angular/common/http";
-import * as data from '../../api/db.json';
+import { HttpClientModule } from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Product} from "../product";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-menu',
@@ -7,10 +9,11 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./menu.component.scss'],
 })
 
-
 export class MenuComponent{
 
-  teams$ = this.http.get<any>(' http://localhost:3000/teams');
-
+  entrees: Observable<Product[]> = this.http.get<Product[]>('/api/entrees');
+  plats: Observable<Product[]> = this.http.get<Product[]>('/api/plats');
+  desserts: Observable<Product[]> = this.http.get<Product[]>('/api/desserts');
+  boissons: Observable<Product[]> = this.http.get<Product[]>('/api/boissons');
   constructor(private http: HttpClient) {}
 }

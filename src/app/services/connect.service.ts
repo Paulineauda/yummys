51249370127f308b51ca5dc8f6f69cbe1adff0fd
Pauline <pauline.auda@free.fr>
2023-11-09 18:30:02 +0100

    connect

--- src/app/services/connect.service.ts
+++ src/app/services/connect.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@angular/core';
 import {Users} from "../users";
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
+import {Product} from "../product";
+import {Avis} from "../avis";
 
 @Injectable({
   providedIn: 'root'
 })
-export class ConnectService {
+export class ConnectService{
+
+  constructor(private http: HttpClient) { }
 
   users: Observable<Users[]> = this.http.get<Users[]>('/api/users');
   isConnected : boolean = false;
-  Identification(email: string, password: string): boolean {
+
+  identification(email: string, password: string): boolean {
     this.users.subscribe((usersArray: Users[]) => {
       for (let user of usersArray) {
+        console.log(this.users)
         if (email === user.email && password === user.password) {
           this.isConnected = true;
           break;
@@ -21,6 +27,4 @@ export class ConnectService {
     });
     return this.isConnected;
   }
-
-  constructor(private http: HttpClient) { }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,36 +1,36 @@
 import {Component} from "@angular/core";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {AuthService} from "./auth.service";
-import {HttpClient} from "@angular/common/http";
+import {ConnectService} from "../services/connect.service";
 import {Observable} from "rxjs";
-import {Avis} from "../avis";
 import {Users} from "../users";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html'
 })
 export class SignupComponent {
-  signUpForm: FormGroup;
 
-  public isConnected : boolean = false;
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  public users: Observable<Users[]> = this.http.get<Users[]>('/api/users');
+  public signUpForm: FormGroup;
+
+  constructor(private fb: FormBuilder, public connectService: ConnectService, private http: HttpClient) {
     this.signUpForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
   }
 
-
-
-
-  onSubmit() {
+  onSubmit() : boolean {
     if (this.signUpForm.valid) {
       const email = this.signUpForm.value.email;
       const password = this.signUpForm.value.password;
-      this.authService.signUp(email, password);
-      // Peut-être rediriger l'utilisateur vers une autre page après l'inscription
+      if (this.connectService.identification(email, password)) {
+        window.alert("connection");
+        return true;
+      }
     }
+    return false;
   }
 
 }
